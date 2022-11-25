b06dc3d5a1d071fb2dbf0190268379802582c4ed
Alex Aragon <alex.aragon@tas-screenit.com>
2022-11-24 22:48:40 -0500

    add yml file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import App from './App';
 import reportWebVitals from './reportWebVitals'
 import Search from './containers/Search'
 import Layout from './containers/Layout'
